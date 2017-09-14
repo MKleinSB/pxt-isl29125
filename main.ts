e4f6f5a02db0109a8ab7337bd67967b61368dba7
Matthias L. Jugel <leo@thinkberg.com>
2017-09-14 15:36:01 +0200

    updated code

--- main.ts
+++ main.ts
@@ -14,8 +14,10 @@ namespace sensors {
     export function isl29125Reset(): void {
         // send to 0x44, register 0x00, value 0x46 (RESET ISL29125)
         pins.i2cWriteNumber(0x44, 0x0046, NumberFormat.UInt16BE);
-        // send to 0x44, register 0x01, value 0x05 (GRB SAMPLING)
+        // send to 0x44, register 0x01, value 0x05 (GRB SAMPLING, 10kLUX)
         pins.i2cWriteNumber(0x44, 0x0105, NumberFormat.UInt16BE);
+        // send to 0x44, register 0x02, value 0x05 (MAX IR FILTER)
+        pins.i2cWriteNumber(0x44, 0x02BF, NumberFormat.UInt16BE);
     }
 
     /**
@@ -27,16 +29,16 @@ namespace sensors {
     export function isl29125Color(): number {
 
         // green
-        pins.i2cWriteNumber(0x44, 0x0A, NumberFormat.Int8BE);
-        let green = pins.i2cReadNumber(0x44, NumberFormat.UInt8BE);
+        pins.i2cWriteNumber(0x44, 0x09, NumberFormat.Int8BE);
+        let green = pins.i2cReadNumber(0x44, NumberFormat.UInt16LE);
 
         // red
-        pins.i2cWriteNumber(0x44, 0x0c, NumberFormat.Int8BE);
-        let red = pins.i2cReadNumber(0x44, NumberFormat.UInt8BE);
+        pins.i2cWriteNumber(0x44, 0x0b, NumberFormat.Int8BE);
+        let red = pins.i2cReadNumber(0x44, NumberFormat.UInt16LE);
 
         // blue
-        pins.i2cWriteNumber(0x44, 0x0e, NumberFormat.Int8BE);
-        let blue = pins.i2cReadNumber(0x44, NumberFormat.UInt8BE);
+        pins.i2cWriteNumber(0x44, 0x0d, NumberFormat.Int8BE);
+        let blue = pins.i2cReadNumber(0x44, NumberFormat.UInt16LE);
 
         serial.writeString("RGB[");
         serial.writeNumber(red);
@@ -44,9 +46,10 @@ namespace sensors {
         serial.writeNumber(green);
         serial.writeString(",");
         serial.writeNumber(blue);
-        serial.writeLine("]");
+        serial.writeString("]\r");
 
-        return basic.rgbw(red, green, blue, 0);
+        // shift colors down (only need the high byte)
+        return basic.rgbw(red >> 8, green >> 8, blue >> 8, 0);
     }
 
 
